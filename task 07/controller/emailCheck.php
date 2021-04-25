00a7925a701eba57f442886d098d362cc23543a2
Ahsun Mahmud Riad <54446121+ahsunriad@users.noreply.github.com>
2021-04-25 12:14:12 +0600

    Add files via upload

---
+++ task 07/controller/emailCheck.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_POST['email'])) {
+    require_once('db.php');
+    require_once '../db/usersServices.php';
+	$emailExists = emailExists($conn, $_POST['email']);
+	if ($emailExists==true) {
+		echo json_encode(array('success'=>false));
+	}else{
+		echo json_encode(array('success'=>true));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
